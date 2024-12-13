749ed4b535f53217a343dd3731494d390947d40b
RodolffoSouza <rodolffo.souza@zup.com.br>
2024-12-13 10:35:18 -0300

    Project setup

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     /*
-    * Desafio: Jogo do Adivinha
-    *  Implemente um jogo interativo no qual o sistema sorteia um número aleatório e o usuário
-    * tenta adivinhar qual é esse número. O jogo deve utilizar condicionais para determinar a
-    * pontuação do usuário com base na proximidade do palpite em relação ao número sorteado.
-    * O objetivo principal é trabalhar a lógica de entrada e saída de dados de forma clara e
-    * coerente.
-    *
-    * Entrega Mínima:
-    * 1. O sistema deve sortear um número aleatório dentro de um intervalo fixo (ex.: 1 a 10).
-    * 2. O usuário deve fornecer um palpite, e o sistema deve comparar o número sorteado com o palpite:
-    * Se o palpite for igual ao número sorteado, o usuário ganha 10 pontos.
-    * Se o palpite estiver 1 unidade acima ou abaixo do número sorteado, o usuário ganha 5 pontos.
-    * Caso contrário, o usuário não ganha pontos.
-    * 3. Ao final de uma rodada, o sistema deve exibir a pontuação total do usuário.
-    * */
+     * Desafio: Jogo do Adivinha
+     *  Implemente um jogo interativo no qual o sistema sorteia um número aleatório e o usuário
+     * tenta adivinhar qual é esse número. O jogo deve utilizar condicionais para determinar a
+     * pontuação do usuário com base na proximidade do palpite em relação ao número sorteado.
+     * O objetivo principal é trabalhar a lógica de entrada e saída de dados de forma clara e
+     * coerente.
+     *
+     * Entrega Mínima:
+     * 1. O sistema deve sortear um número aleatório dentro de um intervalo fixo (ex.: 1 a 10).
+     * 2. O usuário deve fornecer um palpite, e o sistema deve comparar o número sorteado com o palpite:
+     * Se o palpite for igual ao número sorteado, o usuário ganha 10 pontos.
+     * Se o palpite estiver 1 unidade acima ou abaixo do número sorteado, o usuário ganha 5 pontos.
+     * Caso contrário, o usuário não ganha pontos.
+     * 3. Ao final de uma rodada, o sistema deve exibir a pontuação total do usuário.
+     * */
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
 
